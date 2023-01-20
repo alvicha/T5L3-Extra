564c3898f98210f82f7a827175022ebd407d3fb1
Alberto Villegas <alvich@floridauniversitaria.es>
2023-01-20 18:12:53 +0100

    Poniendo ejercicio romano a decimal

---
+++ ejercicioromanoadecimal.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+num=$1
+
+if [[ $num =~ ^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$ ]]; 
+then
+ pos=`expr ${#1} - 1` 
+prev=0 
+result=0 
+while [ $pos -ge 0 ];do 
+	case "${1:$pos:1}"
+	in 
+		M) factor=1000 ;; 
+		D) factor=500 ;; 
+		C) factor=100 ;; 
+		L) factor=50 ;; 
+		X) factor=10 ;; 
+		V) factor=5 ;; 
+		I) factor=1 ;; 
+	esac 
+
+	if [ $factor -lt $prev ];then 
+		result=`expr $result - $factor` 
+	else 
+		result=`expr $result + $factor` 
+	fi 
+	prev=$factor 
+	pos=`expr $pos - 1`
+done
+echo "El numero $1 en romà es $result"
+else
+echo "El número no és correcte o no és ni decimal ni romà"
+fi
